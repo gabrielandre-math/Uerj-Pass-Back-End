159d9ce56de06ce8f244e16a12f4688a8528faae
Gabriel André <gabrieldevpyc@gmail.com>
2025-06-23 21:36:26 -0300

    database: seed com espaço para novos participantes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -21,7 +21,7 @@ async function seed() {
 
   const attendeesToInsert: Prisma.AttendeeUncheckedCreateInput[] = [];
 
-  for (let i = 0; i <= 120; i++) {
+  for (let i = 0; i <= 100; i++) {
     attendeesToInsert.push({
       id: 10000 + i,
       name: faker.person.fullName(),
